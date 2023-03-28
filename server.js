01da36a1044d94ea4187adf1605d9d7d1f894c0c
tejoumayma <oumaymamalak.55@gmail.com>
2023-03-28 20:00:12 +0100

    second

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 mongoose
   .connect(process.env.MONGO_URI, { family: 4 })
   .then(() => console.log("Connected To MongoDB..."))
@@ -14,15 +17,17 @@ mongoose
 app.get("/", async (req, res) => {
   try {
     const user = await User.find();
+    res.send(user);
   } catch (err) {
     console.error("error", err.message);
   }
 });
 
-app.post(" /", async (req, res) => {
+app.post("/", async (req, res) => {
   const { name, email, phone } = req.body;
   try {
     const user = await User.create({ name, email, phone });
+    res.status(201).send(user);
   } catch (err) {
     console.error("error", err.message);
   }
@@ -30,6 +35,10 @@ app.post(" /", async (req, res) => {
 app.put("/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    if (!user) {
+      return res.status(400).send("user not found");
+    }
+    res.send(user);
   } catch (err) {
     console.error("error", err.message);
   }
@@ -37,6 +46,10 @@ app.put("/:id", async (req, res) => {
 app.delete("/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(400).send("user not found");
+    }
+    res.send(user);
   } catch (err) {
     console.error("error", err.message);
   }
